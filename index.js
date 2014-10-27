808f647335660dd792a548e8ba043a6290faf53d
bmwirth09 <bmwirth09@gmail.com>
2014-10-26 23:30:38 -0400

    added an option to have _.template return compiled function, not html

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ var _ = require('lodash');
 var template = _.template;
 
 module.exports = function (data, options) {
+	if (options.returnFunction && data){
+		this.emit('error', new gutil.PluginError('gulp-template', 'Cannot pass data and return a compiled function'));
+	}
+
+	// If undefined is passed in for data to _.template, the compiled function is returned, not html
+	data = (options.returnFunction) ? undefined : data || {};
+
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
 			cb(null, file);
@@ -21,7 +28,7 @@ module.exports = function (data, options) {
 		}
 
 		try {
-			file.contents = new Buffer(template(file.contents.toString(), data || {}, options));
+			file.contents = new Buffer(template(file.contents.toString(), data, options));
 			this.push(file);
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-template', err, {fileName: file.path}));
